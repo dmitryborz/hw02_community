7f3998e6aa9f0c48759c61c2eb743993d574eec8
Дмитрий Борзых <tt7rocky@gmail.com>
2022-03-11 12:44:04 +0300

    5

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,7 +17,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group)[:COUNT_POSTS]
     context = {
         'group': group,
         'posts': posts,
