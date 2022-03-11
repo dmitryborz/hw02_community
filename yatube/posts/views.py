dd45bf780489e0e4ea59ae8922792972df318155
Дмитрий Борзых <tt7rocky@gmail.com>
2022-03-11 11:35:36 +0300

    4.3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-count_posts = 10
+COUNT_POSTS: int = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:count_posts]
+    posts = Post.objects.all()[:COUNT_POSTS]
     # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
@@ -17,7 +17,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
